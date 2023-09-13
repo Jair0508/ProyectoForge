d6ae4024c1de90a0caaa964def7a4c8b38fa62a2
jackgamer1204 <santiagotororoa@gmail.com>
2023-09-13 13:27:23 -0500

    solo agregue una funcion en servicio mensaje

--- src/main/java/com/grupo8/tulibroapp/Servicio/ServicioMensaje.java
+++ src/main/java/com/grupo8/tulibroapp/Servicio/ServicioMensaje.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.grupo8.tulibroapp.Modelos.Mensaje;
+import com.grupo8.tulibroapp.Modelos.Usuario;
 import com.grupo8.tulibroapp.Repositorio.RepositorioMensaje;
 
 @Service
@@ -13,8 +14,12 @@ public class ServicioMensaje extends ServicioBase<Mensaje> {
 
     @Autowired
     private RepositorioMensaje repositorioMensaje;
-    
+
     public List<Mensaje> findMensajesByRemitenteYDestinatario(Long remitenteId, Long destinatarioId) {
         return repositorioMensaje.findMensajesByRemitenteYDestinatario(remitenteId, destinatarioId);
+    }
+
+    public List<Usuario> findUsuariosInteractuados(Usuario usuarioActual) {
+        return repositorioMensaje.findUsuariosInteractuados(usuarioActual);
     }
 }
